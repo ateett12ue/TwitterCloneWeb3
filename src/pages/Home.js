38c667fb1b69a10dbac49c38c7815e29e76958f6
ateet <ateet@propelld.com>
2022-05-25 19:01:12 +0530

    home tweet added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,23 +4,50 @@ import "./Home.css";
 import {defaultImgs} from '../defaultimgs'
 import { TextArea, Icon } from "web3uikit";
 import TweetInFeed from "../components/TweetInFeed";
-
+import { useMoralis } from "react-moralis";
 const Home = () => {
   const inputFile = useRef(null);
   const [selectedFile, setSelectedFile] = useState();
+  const {Moralis} = useMoralis();
+  const user = Moralis.User.current()
+
+  const [theFile, setTheFile] = useState();
+  const [tweet, setTweet] = useState();
+
   const onImageClick= () => {
     inputFile.current.click();
   }
   const changeHandler = (event) => {
     const img = event.target.files[0];
+    setTheFile(img)
     setSelectedFile(URL.createObjectURL(img))
   }
+  const saveTweet = async() => {
+    if(!tweet) return;
+    const Tweets = Moralis.Object.extend("Tweets")
+    const newTweet = new Tweets();
+    newTweet.set("tweetTxt", tweet);
+    newTweet.set("tweeterPfp", user.attributes.pfp);
+    newTweet.set("tweeterAcc", user.attributes.ethAddress);
+    newTweet.set("tweeterUserName", user.attributes.username);
+    
+    if(theFile){
+      const data = theFile;
+      const file = new Moralis.File(data.name, data);
+      await file.saveIPFS();
+      newTweet.set("tweetImg", file.ipfs())
+    }
+
+    await newTweet.save();
+    window.location.reload();
+  }
+
   return (
     <>
     <div className="pageIdentify">Home</div>
      <div className="mainContent">
       <div className="profileTweet">
-        <img src={defaultImgs[0]} className="profilePic" />
+        <img src={user.attributes.pfp ? user.attributes.pfp : defaultImgs[0]} className="profilePic" />
         <div className="tweetBox">
           <TextArea
             label=""
@@ -28,7 +55,7 @@ const Home = () => {
             value="GM World"
             type="text"
             width="95%"
-            
+            onChange={(e)=>setTweet(e.target.value)}
           />
           {
             selectedFile && (
@@ -47,7 +74,7 @@ const Home = () => {
               <Icon fill="#1DA1F2" size={20} svg="image"/>
             </div>
             <div className="tweetOptions">
-              <div className="tweet">Tweet</div>
+              <div className="tweet" onClick={()=>saveTweet()}>Tweet</div>
               <div className="tweet" style={{backgroundColor: "#8247e5"}}>
                 <Icon fill="#ffffff" size={20} svg="matic"/>  
               </div>
