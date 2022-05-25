0419186bb890399ffb54c1cc5fbfb4a7fc08d862
ateet <ateet@propelld.com>
2022-05-25 19:38:11 +0530

    solidity contract added

--- src/components/TweetInFeed.js
+++ src/components/TweetInFeed.js
@@ -1,45 +1,77 @@
-import React from "react";
-import './TweetInFeed.css';
-import {defaultImgs} from "../defaultimgs"
-import golf from "../images/golf.png"
-import {Icon} from "web3uikit"
-const TweetInFeed = () => {
-  
+import React, { useEffect, useState } from "react";
+import "./TweetInFeed.css";
+import { defaultImgs } from "../defaultimgs";
+import golf from "../images/golf.png";
+import { Icon } from "web3uikit";
+import { useMoralis } from "react-moralis";
 
+const TweetInFeed = (profile) => {
+  const { Moralis, account } = useMoralis();
+  const [tweetArr, setTweetArr] = useState();
+
+  useEffect(() => {
+    async function getTweets() {
+      try {
+        const Tweets = Moralis.Object.extend("Tweets");
+        const query = new Moralis.Query(Tweets);
+        if (profile) {
+          query.equalTo("tweeterAcc", account);
+        }
+        const results = await query.find();
+
+        setTweetArr(results);
+        console.log(results);
+      } catch (ex) {
+        console.error(ex);
+      }
+    }
+  }, [profile]);
   return (
     <>
-      <div className="feedTweet">
-        <img src={defaultImgs[0]} className="profilePic"/>
-        <div className="completeTweet">
-        <div className="who">
-          Juhizz
-          <div className="accWhen">
-            0x42..314 - 1h
-          </div>
-          </div>
-          <div className="tweetContent">
-            Nice Day 
-            <img src={golf} className="tweetImg"/>
-          </div>
-          <div className="interactions">
-            <div className="interactionNum">
-              <Icon fill="#3f3f3f" size={20} svg="messageCircle"/>
+      {tweetArr?.map((e) => {
+        return (
+          <div className="feedTweet">
+            <img
+              src={
+                e.attributes.tweeterPfp
+                  ? e.attributes.tweeterPfp
+                  : defaultImgs[0]
+              }
+              className="profilePic"
+            />
+            <div className="completeTweet">
+              <div className="who">
+                {e.attributes.tweeterUserName.slice(0, 6)}
+                <div className="accWhen">{
+                `${e.attributes.tweeterAcc.slice(0,4)}...${e.attributes.tweeterAcc.slice(36)} . 
+                 ${e.attributes.createdAt.toLocalString("en-us", {month: "short"})}
+                 ${e.attributes.createdAt.toLocalString("en-us", {day: "numeric"})}`
+                 }</div>
+              </div>
+              <div className="tweetContent">
+                {e.attributes.tweetTxt}
+                {e.attributes.tweetImg && (
+                  <img src={e.attributes.tweetImg} className="tweetImg" />
+                )}
+              </div>
+              <div className="interactions">
+                <div className="interactionNum">
+                  <Icon fill="#3f3f3f" size={20} svg="messageCircle" />
+                </div>
+                <div className="interactionNum">
+                  <Icon fill="#3f3f3f" size={20} svg="star" />
+                  12
+                </div>
+                <div className="interactionNum">
+                  <Icon fill="#3f3f3f" size={20} svg="matic" />
+                </div>
+              </div>
             </div>
-            <div className="interactionNum">
-              <Icon fill="#3f3f3f" size={20} svg="star"/>
-              12
-            </div>
-            <div className="interactionNum">
-              <Icon fill="#3f3f3f" size={20} svg="matic"/>
-              
-            </div>
           </div>
-          
-        </div>
-      </div>
+        );
+      }).reverse()}
     </>
   );
 };
 
 export default TweetInFeed;
-
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,13 +4,13 @@ import "./Home.css";
 import {defaultImgs} from '../defaultimgs'
 import { TextArea, Icon } from "web3uikit";
 import TweetInFeed from "../components/TweetInFeed";
-import { useMoralis } from "react-moralis";
+import { useMoralis, useWeb3ExecuteFunction } from "react-moralis";
 const Home = () => {
   const inputFile = useRef(null);
   const [selectedFile, setSelectedFile] = useState();
   const {Moralis} = useMoralis();
   const user = Moralis.User.current()
-
+  const contractProccessor = useWeb3ExecuteFunction();
   const [theFile, setTheFile] = useState();
   const [tweet, setTweet] = useState();
 
@@ -21,6 +21,59 @@ const Home = () => {
     const img = event.target.files[0];
     setTheFile(img)
     setSelectedFile(URL.createObjectURL(img))
+  }
+
+  const maticTweet = async() => {
+    if(!tweet) return;
+
+    let img;
+    if(theFile){
+      const data = theFile;
+      const file = new Moralis.File(data.name, data);
+      await file.saveIPFS();
+      img = file.ipfs();
+    }
+    else
+    {
+      img = "No Img"
+    }
+    let options = {
+      contractAddress: "0xd9145CCE52D386f254917e481eB44e9943F39138",
+      functionName: "addTweet",
+      abi: [{
+        "inputs": [
+          {
+            "internalType": "string",
+            "name": "tweetTxt",
+            "type": "string"
+          },
+          {
+            "internalType": "string",
+            "name": "tweetImg",
+            "type": "string"
+          }
+        ],
+        "name": "addTweet",
+        "outputs": [],
+        "stateMutability": "payable",
+        "type": "function"
+      }],
+      params: {
+        tweetTxt: tweet,
+        tweetImg: img,
+      },
+      msgValue: Moralis.Units.ETH(1)
+    }
+
+    await contractProccessor.fetch({
+      params: options,
+      onSuccess: ()=>{
+        saveTweet()
+      },
+      onError:(error)=>{
+        console.log(error.data.message )
+      }
+    })
   }
   const saveTweet = async() => {
     if(!tweet) return;
@@ -75,7 +128,7 @@ const Home = () => {
             </div>
             <div className="tweetOptions">
               <div className="tweet" onClick={()=>saveTweet()}>Tweet</div>
-              <div className="tweet" style={{backgroundColor: "#8247e5"}}>
+              <div className="tweet" onClick={()=>maticTweet()} style={{backgroundColor: "#8247e5"}}>
                 <Icon fill="#ffffff" size={20} svg="matic"/>  
               </div>
             </div>
